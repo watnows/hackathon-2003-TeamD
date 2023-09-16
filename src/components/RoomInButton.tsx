686f762b2f8b6dd1f77aea83a58a288906a92991
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-09-16 23:06:51 +0900

    make file

---
+++ src/components/RoomInButton.tsx
@@ -0,0 +1,10 @@
+const RoomInButton = () => {
+    return(
+        <div className="bg-background text-font text-lg text-3xl p-8 py-10 w-fit rounded-3xl shadow shadow-inner-2xl">
+            <h2>ルームID入力</h2>
+            <input type="text" className="bg-background rounded-tl-2xl rounded-bl-2xl shadow shadow-inner" />
+        </div>
+    )
+}
+
+export default RoomInButton;
