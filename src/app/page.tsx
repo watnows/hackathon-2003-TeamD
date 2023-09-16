ca9438b3ee84af1bf9e9099bb826b1293b2c5cc8
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-09-16 22:14:20 +0900

    make width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-
 export default function Home() {
   return (
-    <main className="min-h-screen p-24">
+    <main className="min-h-screen max-w-[60%] m-auto p-12">
       <h1>hello~~~</h1>
     </main>
   )
