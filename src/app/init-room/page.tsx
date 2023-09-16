b8d5a3bc27ba7d5d5d4ffe8b0c2b9607b33c576a
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-09-17 03:32:22 +0900

    make button

---
+++ src/app/init-room/page.tsx
@@ -0,0 +1,12 @@
+import LongButton from "@/components/LongButton";
+
+const Page = () => {
+    return (
+        <>
+            <h1>Init Room</h1>
+            <LongButton text="ルーム決定" />
+        </>
+    );
+}
+
+export default Page;
