4b2c8d10b10f85cc96690bb75de3d365fc06cdc9
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-09-16 21:07:03 +0900

    make shadow more

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <header className="shadow shadow-inne bg-background">
+        <header className="shadow-md shadow-inne bg-background">
             <h1 className="p-4 pt-6 text-3xl text-font">MoodHub</h1>
         </header>
     )
