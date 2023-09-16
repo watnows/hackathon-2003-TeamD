279e5537dda23be6be91c9390cae26c97690b302
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-09-17 01:29:33 +0900

    complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import CreateRoom from "@/components/CreateRoom"
 import RoomInButton from "@/components/RoomInButton"
 export default function Home() {
   return (
-    <main className="min-h-screen max-w-[60%] m-auto p-12">
+    <main className="min-h-screen md:max-w-[60%] m-auto p-12">
       <RoomInButton />
       <CreateRoom />
     </main>
