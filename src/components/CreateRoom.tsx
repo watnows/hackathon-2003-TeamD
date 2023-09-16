70fad15294e8d523100bfc12b7c22ae0af30478f
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-09-16 22:49:19 +0900

    make create room

---
+++ src/components/CreateRoom.tsx
@@ -0,0 +1,9 @@
+const CreateRoom = () => {
+    return(
+        <div className="bg-background text-font text-lg text-3xl p-8 py-10 w-fit rounded-3xl shadow shadow-inner-2xl">
+            <h2>ルーム作成</h2>
+        </div>
+    )
+}
+
+export default CreateRoom;
