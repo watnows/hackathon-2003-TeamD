4ff9340f47d06f89f2f5802af0ad16b5d7303c5e
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-09-17 01:01:34 +0900

    pudate root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-
+import CreateRoom from "@/components/CreateRoom"
+import RoomInButton from "@/components/RoomInButton"
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="min-h-screen max-w-[60%] m-auto p-24">
       <h1>hello~~~</h1>
+      <RoomInButton />
+      <CreateRoom />
     </main>
   )
 }
