9b43daef56abe6da990ce0b341135fac8235a307
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-09-17 00:03:31 +0900

    fix previous shadow

--- src/components/CreateRoom.tsx
+++ src/components/CreateRoom.tsx
@@ -1,6 +1,6 @@
 const CreateRoom = () => {
     return(
-        <div className="bg-background text-font text-lg text-3xl p-8 py-10 w-fit rounded-3xl shadow shadow-inner-2xl">
+        <div className="bg-background text-font text-lg text-3xl p-8 py-10 w-fit rounded-3xl shadow shadow-boxOut">
             <h2>ルーム作成</h2>
         </div>
     )
