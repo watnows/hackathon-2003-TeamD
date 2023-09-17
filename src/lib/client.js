fb00df01ab05e21202eaf4023b1b55802c6bacba
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-09-17 20:32:35 +0900

    コンソールに出るように

--- src/lib/client.js
+++ src/lib/client.js
@@ -9,7 +9,7 @@ export const { getClient } = registerApolloClient(() => {
     return new NextSSRApolloClient({
         cache: new NextSSRInMemoryCache(),
         link: new HttpLink({
-            uri: "https://main--time-pav6zq.apollographos.net/graphql",
+            uri: "https://mood-hub.onrender.com/graphql",
         }),
     });
 });
