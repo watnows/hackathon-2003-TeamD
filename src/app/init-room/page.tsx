cecbf2fac71291bc235780d1352bb305971ae492
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-09-17 12:06:00 +0900

    make list

--- src/app/init-room/page.tsx
+++ src/app/init-room/page.tsx
@@ -1,9 +1,12 @@
 import LongButton from "@/components/LongButton";
+import NeumourList from "@/components/NeumorList";
 
 const Page = () => {
+    const userList = ["john", "jane", "james", "judy", "jake", "jill", "josh", "jessica", "jackson", "jennifer", "jordan", "julie", "jim", "julian", "juliet", "joseph", "jose", "julio", "juliana", "julius", "juliette", "julianne", "ju"]
     return (
         <>
             <h1>Init Room</h1>
+            <NeumourList listItems={userList} />
             <LongButton text="ルーム決定" />
         </>
     );
