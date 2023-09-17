d591465d2c835aab8246e4bd351d3bbbdb20a7c1
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-09-17 12:07:59 +0900

    add margin

--- src/components/LongButton.tsx
+++ src/components/LongButton.tsx
@@ -5,7 +5,7 @@ interface LongButtonProps {
 const LongButton = (props: LongButtonProps) => {
     const { text } = props;
     return (
-        <button className="bg-background text-font text-lg text-3xl m-auto p-8 py-4 w-full rounded-3xl shadow shadow-boxOut" onClick={() => { }}>
+        <button className="bg-background text-font text-lg text-3xl m-auto p-8 py-4 my-8 w-full rounded-3xl shadow shadow-boxOut" onClick={() => { }}>
             <h2>{text}</h2>
         </button>
     )
